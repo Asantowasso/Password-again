acc0eb92f892af23851836769ab2caa6164cea56
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-10-11 17:33:56 -0400

    linked js file

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+
+function buildQuiz(){
+    const output = [];
+
+    theQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+    
+    const answers = [];
+    
+    for(letter in currentQuestion.answers){
+    answers.push(
+        `<label>
+        <input type="radio" name="question${questionNumber}" value=
+        ${letter} :
+        ${currentQuestion.answers[letter]}
+        </label>`
+    );
+}
+    output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+        );
+    }
+    );
+
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){}
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const theQuestions = [
+    {
+        question: "Which of the following is not a web browser",
+        answers: {
+        a:"Opera",
+        b:"Firefox",
+        c:"Powershell"
+        },
+        correctAnswer: "c"
+    },
+    
+    {
+        question: "What is a GUI",
+        answers: {
+        a: "Good uniform idea",
+        b: "Graphical user interface",
+        c: "Geo united isle " 
+        },
+        correctAnswer: "c"
+    },
+    
+    {
+        question: "Which is not an operating system",
+        answers: {
+        a: "Google",
+        b: "Windows",
+        c: "IOS"
+        },
+        correctAnswer: "a"
+    }
+    ]
+
+
+
+submitButton.addEventListener('click', showResults)
+
+
