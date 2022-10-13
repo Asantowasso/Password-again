1090a8825510e4dc9a2d76fc44de1a7fa5baac35
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-10-12 20:08:29 -0400

    yet more changes

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,129 @@
 
-function buildQuiz(){
-    const output = [];
 
-    theQuestions.forEach(
-    (currentQuestion, questionNumber) => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function buildQuiz(){
+//     const output = [];
+
+//     theQuestions.forEach(
+//     (currentQuestion, questionNumber) => {
     
-    const answers = [];
+//     const answers = [];
     
-    for(letter in currentQuestion.answers){
-    answers.push(
-        `<label>
-        <input type="radio" name="question${questionNumber}" value=
-        ${letter} :
-        ${currentQuestion.answers[letter]}
-        </label>`
-    );
-}
-    output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-        );
-    }
-    );
+//     for(letter in currentQuestion.answers){
+//     answers.push(
+//         `<label>
+//         <input type="radio" name="question${questionNumber}" value=
+//         ${letter} :
+//         ${currentQuestion.answers[letter]}
+//         </label>`
+//     );
+// }
+//     output.push(
+//         `<div class="question"> ${currentQuestion.question} </div>
+//         <div class="answers"> ${answers.join('')} </div>`
+//         );
+//     }
+//     );
 
-    quizContainer.innerHTML = output.join('');
-}
+//     quizContainer.innerHTML = output.join('');
+// }
 
-function showResults(){}
+// function showResults(){
+//     const answerContainers = quizContainer.querySelectorAll('.answers');
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const theQuestions = [
-    {
-        question: "Which of the following is not a web browser",
-        answers: {
-        a:"Opera",
-        b:"Firefox",
-        c:"Powershell"
-        },
-        correctAnswer: "c"
-    },
+//     let numCorrect = 0;
+
+//     myQuestions.forEach((currentQuestion, questionNumber) => {
     
-    {
-        question: "What is a GUI",
-        answers: {
-        a: "Good uniform idea",
-        b: "Graphical user interface",
-        c: "Geo united isle " 
-        },
-        correctAnswer: "c"
-    },
+//     }
+//     )
+// }
+
+// const quizContainer = document.getElementById('quiz');
+// const resultsContainer = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
+// const theQuestions = [
+//     {
+//         question: "Which of the following is not a web browser",
+//         answers: {
+//         a:"Opera",
+//         b:"Firefox",
+//         c:"Powershell"
+//         },
+//         correctAnswer: "c"
+//     },
     
-    {
-        question: "Which is not an operating system",
-        answers: {
-        a: "Google",
-        b: "Windows",
-        c: "IOS"
-        },
-        correctAnswer: "a"
-    }
-    ]
+//     {
+//         question: "What is a GUI",
+//         answers: {
+//         a: "Good uniform idea",
+//         b: "Graphical user interface",
+//         c: "Geo united isle " 
+//         },
+//         correctAnswer: "c"
+//     },
+    
+//     {
+//         question: "Which is not an operating system",
+//         answers: {
+//         a: "Google",
+//         b: "Windows",
+//         c: "IOS"
+//         },
+//         correctAnswer: "a"
+//     }
+//     ]
 
 
 
-submitButton.addEventListener('click', showResults)
+// submitButton.addEventListener('click', showResults)
 
 
